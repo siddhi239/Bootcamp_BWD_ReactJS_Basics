565d4cc6a0d5139eb8f2baae23f4d2296de68d15
Siddhi Gavande <31299309+siddhi239@users.noreply.github.com>
2021-06-05 20:08:16 +0530

    Add files via upload

---
+++ Note.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Note() {
+  return (
+    <div className="note">
+      <h1>Javascript and React.js</h1>
+      <p>
+        Hello there! This is the project of React.js done by Siddhi Gavande to
+        learn and get to know how React.js works.
+      </p>
+    </div>
+  );
+}
+
+export default Note;
